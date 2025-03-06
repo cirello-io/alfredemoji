1cd9d71db5a77350e07ee04e073613e22fef4cb0
Ulderico Cirello <ulderico.cirello@gmail.com>
2025-03-06 14:44:59 -0800

    alfredemoji: upgrade to Unicode 17.0

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 	"strings"
 )
 
-const unicodeVersion = "16.0"
+const unicodeVersion = "17.0"
 const emojiSetURL = "https://unicode.org/Public/emoji/" + unicodeVersion + "/emoji-test.txt"
 
 func main() {
